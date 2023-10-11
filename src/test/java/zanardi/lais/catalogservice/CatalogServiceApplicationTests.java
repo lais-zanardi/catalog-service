6d7873bb009e405148b5d40db929e5ba4b96f617
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-10-11 17:08:39 -0300

    first commit

---
+++ src/test/java/zanardi/lais/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zanardi.lais.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
